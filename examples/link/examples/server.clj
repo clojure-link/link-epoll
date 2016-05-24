75bc227753149495fbae7fb3df6b2dfc14f31e51
Ning Sun <sunng@about.me>
2016-05-24 23:03:29 +0800

    (test) added example code
    Signed-off-by: Ning Sun <sunng@about.me>

---
+++ examples/link/examples/server.clj
@@ -0,0 +1,20 @@
+(ns link.examples.server
+  (:require [link.core :refer :all]
+            [link.codec :as codec]
+            [link.tcp.epoll :as tcp]))
+
+(def line-codec
+  (codec/string :delimiter "\r\n" :encoding :utf8))
+
+(def handler
+  (create-handler
+   (on-message [ch msg]
+               (println (format "%040x" (BigInteger. 1 (.getBytes msg))))
+               (send! ch msg))))
+
+(defn -main [& args]
+  (println "Starting echo server")
+  (tcp/tcp-server 9930 [(codec/netty-encoder line-codec)
+                        (codec/netty-decoder line-codec)
+                        handler])
+  (println "Echo server started on 9930"))
