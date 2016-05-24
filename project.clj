8b2477413d61e06f5328ae80640a4cd325e46bc5
Ning Sun <sunng@about.me>
2016-05-24 19:13:14 +0800

    (feat) initial commit
    Signed-off-by: Ning Sun <sunng@about.me>

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject link/link-epoll "0.1.0-SNAPSHOT"
+  :description "epoll backend for link tcp module"
+  :url "http://github.com/sunng87/link-epoll"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [link "0.8.17"]
+                 [io.netty/netty-transport-native-epoll "4.0.36.Final"
+                  :classifier "linux-x86_64"]])
---
+++ src/link/tcp/epoll.clj
@@ -0,0 +1,66 @@
+(ns link.tcp.epoll
+  (:require [link.core :refer :all]
+            [link.codec :refer [netty-encoder netty-decoder]]
+            [link.tcp :as tcp]
+            [clojure.tools.logging :as logging])
+  (:import [java.net InetAddress InetSocketAddress]
+           [io.netty.bootstrap Bootstrap ServerBootstrap]
+           [io.netty.channel ChannelInitializer Channel ChannelHandler
+            ChannelHandlerContext ChannelFuture EventLoopGroup
+            ChannelPipeline ChannelOption]
+           [io.netty.channel.epoll EpollEventLoopGroup
+            EpollServerSocketChannel EpollSocketChannel]
+           [io.netty.util.concurrent EventExecutorGroup]
+           [link.core ClientSocketChannel]))
+
+(defn- start-tcp-server [host port handlers options]
+  (let [boss-group (EpollEventLoopGroup.)
+        worker-group (EpollEventLoopGroup.)
+        bootstrap (ServerBootstrap.)
+
+        channel-initializer (tcp/channel-init handlers)
+
+        options (group-by #(.startsWith (name (% 0)) "child.") (into [] options))
+        parent-options (get options false)
+        child-options (map #(vector (keyword (subs (name (% 0)) 6)) (% 1)) (get options true))]
+    (doto bootstrap
+      (.group boss-group worker-group)
+      (.channel EpollServerSocketChannel)
+      (.childHandler channel-initializer))
+    (doseq [op parent-options]
+      (.option bootstrap (tcp/to-channel-option (op 0)) (op 1)))
+    (doseq [op child-options]
+      (.childOption bootstrap (tcp/to-channel-option (op 0)) (op 1)))
+
+    (.sync ^ChannelFuture (.bind bootstrap (InetAddress/getByName host) port))
+    ;; return event loop groups so we can shutdown the server gracefully
+    [worker-group boss-group]))
+
+(defn tcp-server [port handlers
+                  & {:keys [options host]
+                     :or {options {}
+                          host "0.0.0.0"}}]
+  (let [handlers (if (sequential? handlers) handlers [handlers])]
+    (start-tcp-server host
+                      port
+                      handlers
+                      options)))
+
+(defn tcp-client-factory [handlers
+                          & {:keys [options]
+                             :or {options {}}}]
+  (let [worker-group (EpollEventLoopGroup.)
+        bootstrap (Bootstrap.)
+        handlers (if (sequential? handlers) handlers [handlers])
+
+        channel-initializer (tcp/channel-init handlers)
+        options (into [] options)]
+
+    (doto bootstrap
+      (.group worker-group)
+      (.channel EpollSocketChannel)
+      (.handler channel-initializer))
+    (doseq [op options]
+      (.option bootstrap (tcp/to-channel-option (op 0)) (op 1)))
+
+    [bootstrap worker-group]))
