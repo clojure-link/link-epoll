956da004d6bd7b71ffb3f4184acde3440da18860
Ning Sun <sunng@about.me>
2016-06-08 11:13:04 +0800

    (chore) netty security update
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [link "0.8.18"]
-                 [io.netty/netty-transport-native-epoll "4.0.36.Final"
+                 [link "0.8.19"]
+                 [io.netty/netty-transport-native-epoll "4.0.37.Final"
                   :classifier "linux-x86_64"]]
   :profiles {:examples {:source-paths ["examples"]}
              :dev {:dependencies [[log4j/log4j "1.2.17"]]}}
