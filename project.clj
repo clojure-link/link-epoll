4604ae8793823020a0b3438fe6b7dec1f475ca73
Ning Sun <sunng@about.me>
2016-07-21 10:57:54 +0800

    (chore) update to netty 4.1
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject link/link-epoll "0.2.3-SNAPSHOT"
+(defproject link/link-epoll "0.3.0-SNAPSHOT"
   :description "epoll backend for link tcp module"
   :url "http://github.com/sunng87/link-epoll"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [link "0.8.20"]
-                 [io.netty/netty-transport-native-epoll "4.0.37.Final"
+                 [link "0.9.0"]
+                 [io.netty/netty-transport-native-epoll "4.1.1.Final"
                   :classifier "linux-x86_64"]]
   :profiles {:examples {:source-paths ["examples"]}
              :dev {:dependencies [[log4j/log4j "1.2.17"]]}}
