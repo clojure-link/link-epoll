037a031c6df3e5554c01dc54f3d549f0daecf77e
Ning Sun <sunng@about.me>
2016-12-13 16:48:17 +0800

    Version 0.3.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject link/link-epoll "0.3.2"
+(defproject link/link-epoll "0.3.3-SNAPSHOT"
   :description "epoll backend for link tcp module"
   :url "http://github.com/sunng87/link-epoll"
   :license {:name "Eclipse Public License"
