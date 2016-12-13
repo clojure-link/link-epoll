0f44c67c9405b37393c107b90761d9c44089e5f3
Ning Sun <sunng@about.me>
2016-12-13 16:47:25 +0800

    (feat) update netty
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [link "0.9.2"]
-                 [io.netty/netty-transport-native-epoll "4.1.5.Final"
+                 [link "0.9.3"]
+                 [io.netty/netty-transport-native-epoll "4.1.6.Final"
                   :classifier "linux-x86_64"]]
   :profiles {:examples {:source-paths ["examples"]}
              :dev {:dependencies [[log4j/log4j "1.2.17"]]}}
