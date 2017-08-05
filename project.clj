9cad9fb305b76f4a796805bf9093c01e39e2b95a
Ning Sun <sunng@about.me>
2017-08-05 15:56:26 +0800

    (chore) update version
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject link/link-epoll "0.3.5-SNAPSHOT"
+(defproject link/link-epoll "0.4.0-SNAPSHOT"
   :description "epoll backend for link tcp module"
   :url "http://github.com/sunng87/link-epoll"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [link "0.9.7"]
-                 [io.netty/netty-transport-native-epoll "4.1.9.Final"
+                 [link "0.10.3-SNAPSHOT"]
+                 [io.netty/netty-transport-native-epoll "4.1.14.Final"
                   :classifier "linux-x86_64"]]
   :profiles {:examples {:source-paths ["examples"]}
              :dev {:dependencies [[log4j/log4j "1.2.17"]]}}
