1a03db0b88fa8dddb3713e461d3591314d745353
ylgrgyq <alven.gr@gmail.com>
2018-09-13 09:17:13 +0800

    Version 0.4.2

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject link/link-epoll "0.4.2-SNAPSHOT"
+(defproject link/link-epoll "0.4.2"
   :description "epoll backend for link tcp module"
   :url "http://github.com/sunng87/link-epoll"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [link "0.12.0-SNAPSHOT"]
+                 [link "0.12.0"]
                  [io.netty/netty-transport-native-epoll "4.1.29.Final"
                   :classifier "linux-x86_64"]]
   :profiles {:examples {:source-paths ["examples"]}
