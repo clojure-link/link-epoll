72f723438b72dc6a2b40eac16a79c50d22ce609c
Ning Sun <sunng@protonmail.com>
2020-05-02 19:02:33 +0800

    (doc) doc up for user timeout

--- examples/link/examples/client.clj
+++ examples/link/examples/client.clj
@@ -22,16 +22,34 @@
   (println "Starting client")
   (let [factory (etcp/tcp-client-factory [(codec/netty-codec line-codec)
                                           handler]
-                                         :options {:epoll.tcp_user_timeout (int 10000)
-                                                   :epoll.tcp_keepidle (int 2)
-                                                   :epoll.tcp_keepintvl (int 3)
-                                                   :epoll.tcp_keepcnt (int 5)
+                                         :options {:epoll.tcp_user_timeout (int 10000) ;; 10 secs
+                                                   :epoll.tcp_keepidle (int 2) ;; 2 secs
+                                                   :epoll.tcp_keepintvl (int 3) ;; 3 secs
+                                                   :epoll.tcp_keepcnt (int 5) ; 5 times
                                                    :so_keepalive true})
         client (tcp/tcp-client factory "127.0.0.1" 9930)]
     (send! client "100\r\n")))
 
+;;
+;; linux is required for this example
+;;
+;; step 0:
+;; start wireshark on loopback, set filter to `tcp.port == 9930`
+;;
+;; step 1:
+;; start server: `lein with-profile examples run -m link.examples.server`
+;;
+;; step 2:
+;; start client: `lein with-profile examples run -m link.examples.client`
+;;
+;; step 3:
 ;; command for dropping packets on loopback
+;; `sudo tc qdisc add dev lo root netem drop 100%`
 ;;
-;; sudo tc qdisc add dev lo root netem drop 100%
+;; step 4:
+;; see the client connection closed
 ;;
+;; step 5:
+;; restore lo
+;; `sudo tc qdisc del dev lo root netem drop 100%`
 ;;
