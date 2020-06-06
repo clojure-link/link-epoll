5c80751118bc0faa649b0b68b69447e5c712947e
Ning Sun <sunng@protonmail.com>
2020-06-06 13:36:48 +0800

    (feat) netty up

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [link "0.12.5"]
-                 [io.netty/netty-transport-native-epoll "4.1.49.Final"
+                 [io.netty/netty-transport-native-epoll "4.1.50.Final"
                   :classifier "linux-x86_64"]]
   :profiles {:examples {:source-paths ["examples"]}
              :dev {:dependencies [[log4j/log4j "1.2.17"]]}}
